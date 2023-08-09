8bda5df11a8d21a1fd47472b4253f5cb57b63228
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-09 18:37:59 +0530

    demo

--- Demo.java
+++ Demo.java
@@ -1,11 +1,15 @@
 public class Demo {
     public static void main(String[] args) {
+        int a=10;
 
 
-        for (int i = 0;  ;i++) {
-
-            System.out.println("hi");
+        for (int i = 0; i<20;i++) {
+            if(i==a){
+                continue;
+            }
+            System.out.println(i);
 
         }
+
     }
 }
