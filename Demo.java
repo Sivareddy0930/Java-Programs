4accd6cbbb3acdf37264a7852e819e57881d8d59
Sivareddy0930 <sivareddy0930@gmail.com>
2023-10-14 13:39:41 +0530

    ok

--- Demo.java
+++ Demo.java
@@ -3,38 +3,42 @@
 import java.util.*;
 
 class Demo {
-    public static int[] merge(int[] arr1, int[] arr2) {
-        int l1 = arr1.length;
-        int l2 = arr2.length;
-        int[] m = new int[l1 + l2];
 
-        int i = 0, j = 0, k = 0;
+    public static boolean StringPer(String s1,String s2) {
 
-        while (i < l1 && j < l2) {
-            if (arr1[i] < arr2[j]) {
-                m[k++] = arr1[i++];
-            } else {
-                m[k++] = arr2[j++];
+
+        int[] arr=new int[500];
+
+
+        for(int i=0;i<s1.length();i++) {
+            arr[(int)s1.charAt(i)]+=1;
+
+        }
+        for(int i=0;i<s2.length();i++) {
+            arr[(int)s2.charAt(i)]-=1;
+        }
+
+
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]!=0) {
+                return false;
             }
         }
+        return true;
 
-        while (i < l1) {
-            m[k++] = arr1[i++];
-        }
 
-        while (j < l2) {
-            m[k++] = arr2[j++];
-        }
-
-        return m;
     }
 
     public static void main(String[] args) {
-        int[] a1={2,5,7,9};
-        int[] a2={1,6,8,2,3,4};
-        int[] b=merge(a1,a2);
-        for(int i=0;i<b.length;i++){
-            System.out.println(b[i]);
-        }
+
+        String s1="abcde";
+        String s2="cdbea";
+
+        String s3="abcde";
+        String s4="cdbaea";
+
+        System.out.println(StringPer(s1,s2));
+        System.out.println(StringPer(s3,s4));
+
     }
 }
--- Demo1.java
+++ Demo1.java
@@ -1,37 +1,13 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
 class Demo1 {
-    public static void merge(int[] arr1, int[] arr2) {
-        int arr[]=new int[arr1.length+arr2.length];
-
-        for (int i = 0; i <arr1.length; i++) {
-            arr[i]=arr1[i];
-        }
-        int count=0;
-        for (int j =arr1.length; j <arr1.length+arr2.length ; j++) {
-            arr[j]=arr2[count];
-            count++;
-        }
-
-        for (int k = 0; k <arr.length; k++) {
-            for (int l =k+1; l <arr.length ; l++) {
-                if(arr[k]>arr[l]){
-                    arr[l]=arr[k]+arr[l];
-                    arr[k]=arr[l]-arr[k];
-                    arr[l]=arr[l]-arr[k];
-                }
-            }
-        }
-
-        for (int a:arr){
-            System.out.println(a);
-        }
-
-
-    }
 
     public static void main(String[] args) {
-        int[] a1={2,5,7,9};
-        int[] a2={1,6,8,2,9,3,4};
-        merge(a1,a2);
+        
+    }
+
 
     }
-}
